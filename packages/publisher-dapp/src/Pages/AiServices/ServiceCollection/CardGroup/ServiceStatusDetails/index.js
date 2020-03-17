737f4724ea5dcbdacf267a7a0a6a79cc9fba2464
SagarikaKatikam <61489562+SagarikaKatikam@users.noreply.github.com>
2020-03-17 19:22:58 +0530

    Daemon config validation

--- packages/publisher-dapp/src/Pages/AiServices/ServiceCollection/CardGroup/ServiceStatusDetails/index.js
+++ packages/publisher-dapp/src/Pages/AiServices/ServiceCollection/CardGroup/ServiceStatusDetails/index.js
@@ -7,7 +7,7 @@ import Tab from "@material-ui/core/Tab";
 import { Link } from "react-router-dom";
 
 import SNETButton from "shared/dist/components/SNETButton";
-
+import AlertBox, { alertTypes } from "shared/dist/components/AlertBox";
 import Revenue from "./Revenue";
 import Usage from "./Usage";
 import Pricing from "./Pricing";
@@ -18,7 +18,8 @@ import { ConfigurationServiceRequest } from "../../../../../Utils/Daemon/Configu
 const ServiceStatusDetails = props => {
   const { classes, status, groups, editServiceLink } = props;
   const [activeTab] = useState(2);
-
+  const [alert, setAlert] = useState({});
+  const [setinvalidValues] = useState([]);
   const tabs = [
     { name: "Revenue", activeIndex: 0, component: <Revenue /> },
     { name: "Usage", activeIndex: 1, component: <Usage /> },
@@ -26,14 +27,37 @@ const ServiceStatusDetails = props => {
     { name: "Changelog", activeIndex: 3, component: <Changelog /> },
   ];
 
+  const configValidation = [
+    ["blockchain_enabled", "false"],
+    ["ipfs_end_point", "http://ipfs.singularitynet.io:80"],
+    ["blockchain_network_selected", "ropsten"],
+    ["passthrough_enabled", "false"],
+    ["organization_id", "sohit1"],
+    ["service_id", "sohit1"],
+    ["daemon_end_point", "0.0.0.0:8083"],
+    ["passthrough_endpoint", "http://localhost:7003"],
+  ];
+
   const activeComponent = tabs.find(el => el.activeIndex === activeTab);
-
   // TODO use the appropriate endpoint of the service's daemon
-  const validateDaemonConfig = async (serviceEndpoint = "https://example-service-a.singularitynet.io:8083") => {
+  const validateDaemonConfig = async () => {
+    const serviceEndpoint = "https://example-service-a.singularitynet.io:8083";
     const configurationServiceRequest = new ConfigurationServiceRequest(serviceEndpoint);
-    await configurationServiceRequest.getConfiguration();
-  };
+    const res = await configurationServiceRequest.getConfiguration();
 
+    res.currentConfigurationMap.map(item => {
+      configValidation.map(item1 => {
+        if (item[0] === item1[0]) {
+          if (item[1] !== item1[1]) {
+            setinvalidValues([item1[0], item1[1]]);
+            setAlert({ type: alertTypes.ERROR, message: item1[0] + " should be " + item1[1] });
+          }
+        }
+        return null;
+      });
+      return setinvalidValues;
+    });
+  };
   return (
     <div className={classes.serviceStatusDetailsMainContainer}>
       <div>
@@ -57,7 +81,11 @@ const ServiceStatusDetails = props => {
           <SNETButton children="edit" color="primary" variant="contained" />
         </Link>
         <SNETButton children="pause service" color="primary" variant="contained" />
-        <SNETButton children="validate daemon" color="primary" variant="contained" onClick={validateDaemonConfig} />
+        <br />
+        {props.status === "APPROVED" ? (
+          <SNETButton children="validate daemon" color="primary" variant="contained" onClick={validateDaemonConfig} />
+        ) : null}
+        <AlertBox type={alert.type} message={alert.message} />
       </div>
     </div>
   );
